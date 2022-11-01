d47720b5e2c7d4df176b04855a1972b069a8e6d4
Karatasdilara <116079552+Karatasdilara@users.noreply.github.com>
2022-11-01 23:10:00 +0300

    Add files via upload

---
+++ invent.py
@@ -0,0 +1,21 @@
+
+
+# 02200201081 DİLARA KARATAŞ #
+
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+
+image = cv2.imread('x_ray.jpg',0)
+[h,w] = image.shape
+new_image = np.zeros([h,w], dtype=np.uint8)
+for i in range(h):
+    for j in range(w):
+        new_image[i,j] = 255 - image[i,j]
+
+print(image[0,0])
+cv2.imshow("Manuel_inverted",new_image)
+cv2.waitKey()
+
